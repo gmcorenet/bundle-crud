61e22fcd8db7e0a0faf90bdb97e489cda0c1615a
GMCoreNET <dev@dev.gmcore.net>
2026-05-07 09:22:22 +0000

    refactor(crud): migrate template functions to global templating.RegisterFunc, remove internal/twig

--- crud.go
+++ crud.go
@@ -2,9 +2,13 @@ package crud
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
 	"html/template"
 	"net/http"
 
+	gmcore_crud_sdk "github.com/gmcorenet/sdk/gmcore-crud"
 	gmcore_bundle "github.com/gmcorenet/sdk/gmcore-bundle"
 	gmcore_templating "github.com/gmcorenet/sdk/gmcore-templating"
 	"github.com/gmcorenet/bundle-crud/internal/controller"
@@ -13,7 +17,6 @@ import (
 	"github.com/gmcorenet/bundle-crud/internal/model"
 	"github.com/gmcorenet/bundle-crud/internal/registry"
 	"github.com/gmcorenet/bundle-crud/internal/service"
-	"github.com/gmcorenet/bundle-crud/internal/twig"
 )
 
 var (
@@ -21,8 +24,7 @@ var (
 	ConfigDefaults   *model.ConfigDefaults
 	FilterRegistry   *filter.FilterTypeRegistry
 	ExporterRegistry *export.ExportRegistry
-	CrudTwigRuntime  *twig.CrudRuntime
-	CrudTwigExtension *twig.CrudExtension
+	crudEngine       *gmcore_templating.Engine
 )
 
 func init() {
@@ -30,6 +32,13 @@ func init() {
 	ConfigDefaults = model.NewConfigDefaults()
 	FilterRegistry = filter.NewFilterTypeRegistry()
 	ExporterRegistry = export.NewExportRegistry()
+
+	gmcore_templating.RegisterFunc("gmcore_crud", func(resource string, withAssets bool) template.HTML {
+		return showCrud(context.Background(), resource, withAssets, "anonymous")
+	})
+	gmcore_templating.RegisterFunc("gmcore_crud_as", func(resource string, withAssets bool, userIdentifier string) template.HTML {
+		return showCrud(context.Background(), resource, withAssets, userIdentifier)
+	})
 }
 
 type Bundle struct {
@@ -39,8 +48,6 @@ type Bundle struct {
 	FilterRegistry   *filter.FilterTypeRegistry
 	ExporterRegistry *export.ExportRegistry
 	controllers      *controller.CrudRouter
-	twigRuntime      *twig.CrudRuntime
-	twigExtension    *twig.CrudExtension
 }
 
 func NewBundle() *Bundle {
@@ -76,24 +83,7 @@ func (b *Bundle) Boot(ctx context.Context) error {
 }
 
 func (b *Bundle) InitTemplating(engine *gmcore_templating.Engine) {
-	b.twigRuntime = twig.NewCrudRuntime(Registry, engine, ConfigDefaults, "gmcore")
-	b.twigExtension = twig.NewCrudExtension(b.twigRuntime)
-	CrudTwigRuntime = b.twigRuntime
-	CrudTwigExtension = b.twigExtension
-}
-
-func (b *Bundle) TemplateFunctions() map[string]interface{} {
-	if b.twigRuntime == nil {
-		return nil
-	}
-	funcs := make(map[string]interface{})
-	funcs["gmcore_crud"] = func(resource string, withAssets bool) template.HTML {
-		return b.twigRuntime.ShowCrud(context.Background(), resource, withAssets, "anonymous")
-	}
-	funcs["gmcore_crud_as"] = func(resource string, withAssets bool, userIdentifier string) template.HTML {
-		return b.twigRuntime.ShowCrud(context.Background(), resource, withAssets, userIdentifier)
-	}
-	return funcs
+	crudEngine = engine
 }
 
 func (b *Bundle) Shutdown() error {
@@ -114,4 +104,102 @@ func (b *Bundle) GetResource(name string) (registry.CrudResource, error) {
 
 func (b *Bundle) AllResources() []registry.CrudResource {
 	return Registry.All()
+}
+
+func showCrud(ctx context.Context, resource string, withAssets bool, userIdentifier string) template.HTML {
+	if crudEngine == nil || Registry == nil {
+		return template.HTML("")
+	}
+
+	crudRes, err := Registry.Get(resource)
+	if err != nil {
+		return template.HTML("")
+	}
+
+	cfg := crudRes.Config()
+
+	if !crudRes.Can("view", nil, nil) {
+		return template.HTML("")
+	}
+
+	params := gmcore_crud_sdk.ListParams{
+		Page:    1,
+		PerPage: 25,
+	}
+
+	records, listErr := crudRes.List(params, nil)
+	if listErr != nil {
+		return template.HTML(fmt.Sprintf("<!-- CRUD list error: %v -->", listErr))
+	}
+
+	instanceID := resource + "-" + randomHex(6)
+
+	payload := map[string]interface{}{
+		"config":            cfg,
+		"crudInstanceId":    instanceID,
+		"params":            params,
+		"result":            buildCrudListResult(records, params),
+		"filterSummaries":   []interface{}{},
+		"crudBundleVersion": "1.0.0",
+		"crudThemeVersion":  "gmcore dev",
+		"crudTheme": map[string]interface{}{
+			"rootClass":   "gmcrud-theme-gmcore",
+			"footerLabel": "gmcore dev",
+		},
+		"crudConfigDefaults": ConfigDefaults,
+		"withAssets":         withAssets,
+		"userIdentifier":     userIdentifier,
+	}
+
+	rendered, renderErr := crudEngine.RenderContext(ctx, "crud/_partials/show_crud.html", payload)
+	if renderErr != nil {
+		return template.HTML(fmt.Sprintf("<!-- CRUD render error: %v -->", renderErr))
+	}
+
+	return template.HTML(rendered)
+}
+
+func buildCrudListResult(records []gmcore_crud_sdk.Record, params gmcore_crud_sdk.ListParams) map[string]interface{} {
+	displayRows := make([]map[string]interface{}, 0, len(records))
+	for _, rec := range records {
+		id, _ := rec["id"]
+		cells := make([]map[string]interface{}, 0)
+		for k, v := range rec {
+			if k == "id" || k == "ID" {
+				continue
+			}
+			cells = append(cells, map[string]interface{}{
+				"field":    k,
+				"value":    v,
+				"labelKey": k,
+			})
+		}
+		displayRows = append(displayRows, map[string]interface{}{
+			"id":      id,
+			"cells":   cells,
+			"actions": []interface{}{},
+		})
+	}
+
+	total := len(records)
+	totalPages := (total + params.PerPage - 1) / params.PerPage
+	if totalPages < 1 {
+		totalPages = 1
+	}
+
+	return map[string]interface{}{
+		"displayRows":   displayRows,
+		"total":         total,
+		"totalPages":    totalPages,
+		"page":          params.Page,
+		"perPage":       params.PerPage,
+		"hasExactTotal": true,
+		"hasMore":       false,
+	}
+}
+
+func randomHex(n int) string {
+	b := make([]byte, n)
+	_, _ = rand.Read(b)
+	return hex.EncodeToString(b)
 }
